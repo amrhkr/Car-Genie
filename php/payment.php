a5487bad3228ea9f74c647abe26c5d11371163cf
Amar Choudhary <amarhaker@gmail.com>
2023-09-21 23:31:29 +0530

    update

--- php/payment.php
+++ php/payment.php
@@ -20,8 +20,10 @@
 <body>
     <div id="includeHeader"></div>
     <div id="commonMenu"></div>
+    <!-- <div class="payment"> -->
+    <center>
     <div class="payment">
-        <form action="accept_payment.php" method="post">
+        <!-- <form action="accept_payment.php" method="post">
             <h1>Payment</h1>
             VEHICLE ID:<input type="text" name="vchl_id" placeholder="Vehicle ID"><br><br> 
             PURCHASE QUENTITY :<input type="number" name="purch_qunt" placeholder="Quentity"><br><br>
@@ -38,8 +40,44 @@
 
             <input type="submit" value="PayNow">
             <input type="submit" value="Reset">
-        </form>
+        </form> -->
+        
+            <form action="accept_payment.php" method="post">
+                <fieldset>
+                    <legend>Payment</legend>
+
+                    <label for="vchl_id">VEHICLE ID:</label>
+                    <input type="text" name="vchl_id" id="vchl_id" placeholder="Vehicle ID"><br><br>
+
+                    <label for="purch_qunt">PURCHASE QUANTITY:</label>
+                    <input type="number" name="purch_qunt" id="purch_qunt" placeholder="Quantity"><br><br>
+
+                    <label for="vchl_nme">VEHICLE NAME:</label>
+                    <input type="text" name="vchl_nme" id="vchl_nme" placeholder="Vehicle NAME"><br><br>
+
+                    <label for="amt">TOTAL AMOUNT:</label>
+                    <input type="text" name="amt" id="amt" placeholder="Amount"><br><br>
+
+                    <label for="pay">Payment Mode:</label>
+                    <select name="pay" id="pay">
+                        <option value="Credit Card">Credit card</option>
+                        <option value="Debit Card">Debit card</option>
+                        <option value="Paytm">Paytm</option>
+                    </select><br><br>
+
+                    <label for="crd_num">CARD NO:</label>
+                    <input type="text" name="crd_num" id="crd_num" placeholder="Card Number"><br><br>
+
+                    <label for="addrs">ADDRESS:</label>
+                    <input type="text" name="addrs" id="addrs" placeholder="Address"><br><br>
+                    <input type="reset" value="Reset">
+                    <input type="submit" value="Pay Now">
+                    
+                </fieldset>
+            </form>
+        
     </div>
+    </center>
     <div id="includeFooter"></div>
 </body>
 
